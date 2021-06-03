af655ac1ca794d1823cde8ddaabc0c05dc6bc3f7
Michał Suszko <michal.suszko@dry.pl>
2021-06-03 14:05:43 +0200

    Fine-tuning

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ dotfiles_dir=`dirname $self`
 mkdir -p \
   $HOME/.config/nvim \
   $HOME/.config/pudb \
+  ${GNUPGHOME:-$HOME/.gnupg} \
   $HOME/.vim/scripts
 ln -s $dotfiles_dir/.zshrc $HOME/.zshrc
 ln -s $dotfiles_dir/.tmux.conf $HOME/.tmux.conf
@@ -14,3 +15,4 @@ ln -s $dotfiles_dir/.vim/rc $HOME/.vimrc
 ln -s $dotfiles_dir/.vim/scripts/python.vim $HOME/.vim/scripts/python.vim
 ln -s $dotfiles_dir/.config/pudb/pudb.cfg $HOME/.config/pudb/pudb.cfg
 ln -s $dotfiles_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $dotfiles_dir/gpg.conf ${GNUPGHOME:-$HOME/.gnupg}/gpg.conf
