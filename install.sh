107d6bb972c5da50ee5dc0f89200ac0c261a44a0
Michał Suszko <michal.suszko@vertexinc.com>
2022-06-07 14:04:11 +0200

    set path to repo, add psql config

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-self=`realpath $0`
-dotfiles_dir=`dirname $self`
+dotfiles_dir=$HOME/git/dotfiles
 
 mkdir -p \
   $HOME/.config/nvim \
   $HOME/.config/pudb \
   ${GNUPGHOME:-$HOME/.gnupg} \
   $HOME/.vim/scripts
-ln -s $dotfiles_dir/.zshrc $HOME/.zshrc
-ln -s $dotfiles_dir/.tmux.conf $HOME/.tmux.conf
-ln -s $dotfiles_dir/.gitconfig $HOME/.gitconfig
-ln -s $dotfiles_dir/.vim/rc $HOME/.vimrc
-ln -s $dotfiles_dir/.vim/scripts/python.vim $HOME/.vim/scripts/python.vim
-ln -s $dotfiles_dir/.config/pudb/pudb.cfg $HOME/.config/pudb/pudb.cfg
-ln -s $dotfiles_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -s $dotfiles_dir/gpg.conf ${GNUPGHOME:-$HOME/.gnupg}/gpg.conf
+ln -sf $dotfiles_dir/.zshrc $HOME/.zshrc
+ln -sf $dotfiles_dir/.tmux.conf $HOME/.tmux.conf
+ln -sf $dotfiles_dir/.gitconfig $HOME/.gitconfig
+ln -sf $dotfiles_dir/.vim/rc $HOME/.vimrc
+ln -sf $dotfiles_dir/.plsqlrc $HOME/.plsqlrc
+ln -sf $dotfiles_dir/.vim/scripts/python.vim $HOME/.vim/scripts/python.vim
+ln -sf $dotfiles_dir/.config/pudb/pudb.cfg $HOME/.config/pudb/pudb.cfg
+ln -sf $dotfiles_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
