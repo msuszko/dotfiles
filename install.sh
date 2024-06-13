f43dcb79a9a7de8b7324311ac94869a33c5d64bc
Michał Suszko <michal.suszko@vertexinc.com>
2024-06-13 20:53:06 +0200

    old GnuPG config

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ ln -sf $dotfiles_dir/.vim/scripts/python.vim $HOME/.vim/scripts/python.vim
 ln -sf $dotfiles_dir/.config/pudb/pudb.cfg $HOME/.config/pudb/pudb.cfg
 ln -sf $dotfiles_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $dotfiles_dir/.config/starship.toml $HOME/.config/starship.toml
+ln -sf $dotfiles_dir/gpg.conf ${GNUPGHOME:-$HOME/.gnupg}/gpg.conf
