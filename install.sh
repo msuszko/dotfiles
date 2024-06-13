231e8d893c34309b49f49658d77286f5246379ed
Michał Suszko <michal.suszko@vertexinc.com>
2024-06-13 21:03:59 +0200

    alacritty config, vim-plug install

--- install.sh
+++ install.sh
@@ -17,3 +17,11 @@ ln -sf $dotfiles_dir/.config/pudb/pudb.cfg $HOME/.config/pudb/pudb.cfg
 ln -sf $dotfiles_dir/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $dotfiles_dir/.config/starship.toml $HOME/.config/starship.toml
 ln -sf $dotfiles_dir/gpg.conf ${GNUPGHOME:-$HOME/.gnupg}/gpg.conf
+ln -sf $dotfiles_dir/.config/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+
+# Install vim-plug
+if [ ! -f ~/.vim/autoload/plug.vim ]
+then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
